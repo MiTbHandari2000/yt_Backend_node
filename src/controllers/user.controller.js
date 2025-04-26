494942e402cc861de80c2376150c48940ebf94aa
mit bhandari <bhandari.meet007@gmail.com>
2025-04-26 12:08:57 +0530

    update response format in getCurrentUser and fix variable declaration in updateAccountDetails

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -318,7 +318,7 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 const getCurrentUser = asyncHandler(async (req, res) => {
   return res
     .status(200)
-    .json(200, req.user, "current user fetched successfully");
+    .json(new ApiResponse(200, req.user, "current user fetched successfully"));
 });
 
 /------------------------update account access -------------/;
@@ -330,7 +330,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  await User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {
