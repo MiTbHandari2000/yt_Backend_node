4cfa2bf856f5610e8139b83c4708edc2bda70309
mit bhandari <bhandari.meet007@gmail.com>
2025-05-21 14:25:58 +0530

    fix: correct import path for asyncHandler in multiple controller files

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -3,7 +3,7 @@ import { Playlist } from "../models/playlist.model.js";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandaler.js";
 
 /--------------------TODO: create playlist-------------------/;
 const createPlaylist = asyncHandler(async (req, res) => {
--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -5,7 +5,7 @@ import { Like } from "../models/like.model.js";
 import { Comment } from "../models/comment.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandaler.js";
 import {
   uploadOnCloudinary,
   deleteFromCloudinary,
